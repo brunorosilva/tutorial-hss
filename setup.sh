796a73cd38678cc698ceafe0840e2473a0834220
Bruno Rodrigues Silva <b.rosilva1@gmail.com>
2020-09-01 23:40:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.streamlit/
 echo "[general]
-email = "rodriguessilvabruno@outlook.com"
+email = \"rodriguessilvabruno@outlook.com\"
 " > ~/.streamlit/credentials.toml
 echo "[server]
 headless = true
